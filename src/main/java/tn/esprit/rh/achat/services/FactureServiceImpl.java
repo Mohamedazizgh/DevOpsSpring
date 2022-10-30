a338c2e429ab1008eb3d79f44faa2910ffb88bc9
Mhamed Aziz Ghorbel <47769181+Mohamedazizgh@users.noreply.github.com>
2022-10-30 23:24:16 +0100

    Update FactureServiceImpl.java

--- src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
@@ -20,8 +20,7 @@ public class FactureServiceImpl implements IFactureService {
 	
 	@Autowired
 	DetailFactureRepository detailFactureRepository;
-	@Autowired
-	FournisseurRepository fournisseurRepository;
+
 	
     @Autowired
     ReglementServiceImpl reglementService;
@@ -49,7 +48,7 @@ public class FactureServiceImpl implements IFactureService {
 		float montantRemise = 0;
 		for (DetailFacture detail : detailsFacture) {
 			//Récuperer le produit 
-			Produit produit = produitRepository.findById(detail.getProduit().getIdProduit()).get();
+			
 			//Calculer le montant total pour chaque détail Facture
 			float prixTotalDetail = detail.getQteCommandee() * produit.getPrix();
 			//Calculer le montant remise pour chaque détail Facture
