9a4ae9006a49c512d471a535be0db765e4c04edf
Mhamed Aziz Ghorbel <47769181+Mohamedazizgh@users.noreply.github.com>
2022-10-30 23:08:00 +0100

    Update DetailFacture.java

--- src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
+++ src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
@@ -26,8 +26,7 @@ public class DetailFacture implements Serializable {
 	private float prixTotalDetail;
 	private Integer pourcentageRemise;
 	private float montantRemise;
-	@ManyToOne
-	private Produit produit;
+	
 	@ManyToOne
 	@JsonIgnore
 	Facture facture;
