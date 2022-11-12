88142e41bf0451246a025a3f35c478b8a48e173f
Mhamed Aziz Ghorbel <47769181+Mohamedazizgh@users.noreply.github.com>
2022-11-12 15:55:24 +0100

    Update FactureServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/FactureServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/FactureServiceTest.java
@@ -1,87 +1,96 @@
-/*package tn.esprit.rh.achat.services;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+package tn.esprit.rh.achat.services;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
+
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import tn.esprit.rh.achat.entities.Facture;
-import tn.esprit.rh.achat.repositories.FactureRepository;
-import tn.esprit.rh.achat.services.FactureServiceImpl;
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.entities.DetailFacture;
+import com.esprit.examen.entities.Facture;
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.CategorieProduitRepository;
+import com.esprit.examen.repositories.DetailFactureRepository;
+import com.esprit.examen.repositories.FactureRepository;
+import com.esprit.examen.repositories.ProduitRepository;
 
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
-public class FactureServiceTest {
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class FactureServiceImplTest {
+    
     @Mock
     FactureRepository factureRepository;
-
+    @Mock
+    DetailFactureRepository detailFactureRepository;
+    
+    @Mock
+    ProduitRepository produitRepository;
+    
+    @Mock
+    CategorieProduitRepository categorieProduitRepository;
+    
     @InjectMocks
-    FactureServiceImpl factureService;
+    FactureServiceImpl factureServiceImpl;
+    
+    @InjectMocks
+    ProduitServiceImpl produitServiceImpl;
+    
+    @InjectMocks
+    CategorieProduitServiceImpl categorieProduitServiceImpl;
+    
 
+    
     @Test
-    public void testRetrieveFacture() {
+    public void sommeFactureTest() {
+        log.info("test : sommeFactureTest" );
 
-        Facture facture = new Facture(1L, 100, 500, null, null, null, null, null, null);
+        Float sum = 0F;
 
-        facture.setIdFacture(1L);
+        CategorieProduit cProduit = CategorieProduit.builder().codeCategorie("code1")
+                .libelleCategorie("libelle categ").idCategorieProduit(1L).build();
+        Mockito.when(categorieProduitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(cProduit));
+        Stock stock = Stock.builder().libelleStock("lib stock").qte(10).qteMin(50).idStock(1L).build();
+        
+        Produit p1 = Produit.builder().codeProduit("code1").libelleProduit("lib1").prix(50.9F).categorieProduit(cProduit).stock(stock)
+                .idProduit(1L).build();
+        Mockito.when(produitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(p1));
+        p1.setCategorieProduit(cProduit);
+        DetailFacture detailFacture = DetailFacture.builder().montantRemise(0F).pourcentageRemise(0).qteCommandee(9).produit(p1).build();
+        Facture facture = Facture.builder().dateCreationFacture(new Date()).build();
+        HashSet<DetailFacture> detailsList = new HashSet<DetailFacture>();
+        detailsList.add(detailFacture);
+        
+        facture = factureServiceImpl.addDetailsFacture(facture, detailsList);
 
-        Mockito.when(factureRepository.findById(1L)).thenReturn(Optional.of(facture));
-        factureService.retrieveFacture(1L);
-        Assertions.assertNotNull(facture);
+       
+        Mockito.when(factureRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(facture));
+        
+        for (DetailFacture detailFacture2 : detailsList) {
+            sum += detailFacture2.getProduit().getPrix()* detailFacture2.getQteCommandee();
+        }
 
-        System.out.println(facture);
-        System.out.println(" Retrieve is working correctly...!!");
+        log.info("expected sum="+ sum );
+        log.info("actual sum="+ facture.getMontantFacture() );
+        log.info("test : finished sommeFactureTest" );
 
+
+        assertEquals(sum, facture.getMontantFacture());
     }
 
-
-  /*  @Test
-    public void createFacturekTest()
-    {
-        Facture facture2 = new Facture(2L, 100, 500, null, null, null, null, null, null);
-        facture2.setIdFacture(2L);
-
-        factureService.addFacture(facture2);
-        //verify(factureRepository, times(1)).save(facture2);
-        System.out.println(facture2);
-        System.out.println(" Create is working correctly...!!");
-    }*/
-
-
-   /* @Test
-    public void getAllFactureTest()
-    {
-        List<Facture> Facturelist = new ArrayList<Facture>() {
-
-            {
-                add(new Facture(3L, 100, 700, null, null, null, null, null, null));
-                add(new Facture(4L, 200, 800, null, null, null, null, null, null));
-                add(new Facture(5L, 300, 900, null, null, null, null, null, null));
-            }};
-
-
-        when(factureService.retrieveAllFactures()).thenReturn(Facturelist);
-        //test
-        List<Facture> factureList = factureService.retrieveAllFactures();
-        assertEquals(3, factureList.size());
-        System.out.println(" Retrieve all is working correctly...!!");
-    }/*
-
-
-
-
-}*/
+}
