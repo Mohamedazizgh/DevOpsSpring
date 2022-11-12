cb26478caccba7bd1554e6e30ceecdc086fb2ce7
Mhamed Aziz Ghorbel <47769181+Mohamedazizgh@users.noreply.github.com>
2022-11-12 15:18:40 +0100

    Update FactureServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/FactureServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/FactureServiceTest.java
@@ -51,7 +51,7 @@ public class FactureServiceTest {
 
 
     @Test
-    public void createFacturekTest()
+    public void createFactureTest()
     {
         Facture facture2 = new Facture(2L, 100, 500, null, null, null, null, null, null);
         facture2.setIdFacture(2L);
