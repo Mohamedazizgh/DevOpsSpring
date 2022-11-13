07f1458d1ebf34171d93e63fbf43d9603e512937
Mhamed Aziz Ghorbel <47769181+Mohamedazizgh@users.noreply.github.com>
2022-11-13 22:23:45 +0100

    Update DetailFactureRepository.java

--- src/main/java/tn/esprit/rh/achat/repositories/DetailFactureRepository.java
+++ src/main/java/tn/esprit/rh/achat/repositories/DetailFactureRepository.java
@@ -6,16 +6,6 @@ import tn.esprit.rh.achat.entities.DetailFacture;
 
 @Repository
 public interface DetailFactureRepository extends JpaRepository<DetailFacture, Long> {
-  @Query("SELECT f FROM Facture f where f.fournisseur=:fournisseur and f.archivee=false")
-	public List<Facture> getFactureByFournisseur(@Param("fournisseur") Fournisseur fournisseur);
-
-	
-	@Query("SELECT sum(f.montantFacture) FROM Facture f where  f.dateCreationFacture between :startDate"
-			+ " and :endDate and f.archivee=false")
-	float getTotalFacturesEntreDeuxDates(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
-
-	@Modifying
-	@Query("update Facture f set f.archivee=true where f.idFacture=?1")
-	void updateFacture(Long id);
+  
 
 }
