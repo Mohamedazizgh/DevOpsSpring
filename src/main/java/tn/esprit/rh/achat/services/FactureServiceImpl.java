40ee5f121548f580b2a8c7d188c1ea998eb73d27
Mhamed Aziz Ghorbel <47769181+Mohamedazizgh@users.noreply.github.com>
2022-11-14 17:03:38 +0100

    Update FactureServiceImpl.java

--- src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
@@ -47,7 +47,7 @@ public class FactureServiceImpl implements IFactureService {
 	 * calculer les montants remise et le montant total d'un détail facture
 	 * ainsi que les montants d'une facture
 	 */
-	private Facture addDetailsFacture(Facture f, Set<DetailFacture> detailsFacture) {
+	/*private Facture addDetailsFacture(Facture f, Set<DetailFacture> detailsFacture) {
 		float montantFacture = 0;
 		float montantRemise = 0;
 		for (DetailFacture detail : detailsFacture) {
@@ -69,7 +69,7 @@ public class FactureServiceImpl implements IFactureService {
 		f.setMontantFacture(montantFacture);
 		f.setMontantRemise(montantRemise);
 		return f;
-	}
+	}*/
 
 	@Override
 	public void cancelFacture(Long factureId) {
