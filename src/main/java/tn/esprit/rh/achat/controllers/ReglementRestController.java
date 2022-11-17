32aaca2bf4e5e0ca60d8ccb68709ea96426e1be3
Snoussi22 <73795951+Snoussi22@users.noreply.github.com>
2022-11-17 21:18:20 +0100

    Update ReglementRestController.java

--- src/main/java/tn/esprit/rh/achat/controllers/ReglementRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/ReglementRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @Api(tags = "Gestion des reglements")
 @RequestMapping("/reglement")
-@CrossOrigin("http://192.168.1.13:8089/SpringMVC/")
+@CrossOrigin("*")
 public class ReglementRestController {
 
     @Autowired
